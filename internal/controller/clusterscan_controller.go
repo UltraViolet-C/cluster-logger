d3b3a3df830a76dd40ed82404ac3937fed6c19b4
UltraViolet-C <yuvraajchahil9@gmail.com>
2024-05-16 23:32:09 -0400

    Finished implementing controller

--- internal/controller/clusterscan_controller.go
+++ internal/controller/clusterscan_controller.go
@@ -18,8 +18,15 @@ package controller
 
 import (
 	"context"
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
 	"time"
 
+	kbatch "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -42,6 +49,47 @@ func (_ realClock) Now() time.Time { return time.Now() }
 // mocked out so timing can be faked during tests
 type Clock interface {
 	Now() time.Time
+}
+
+func prettyPrintClusterInfo(clusterScan logv1.ClusterScan) string {
+	var output string = fmt.Sprintf(">>> Cluster Scan made at time %s <<<\n", time.Now().String()) +
+		fmt.Sprintf("Version: %s | Name: %s\n", clusterScan.Spec.Version, clusterScan.Spec.Name) +
+		"Nodes:\n"
+
+	var nameColumnWidth int = len("Name")
+	var uidColumnWidth int = len("UID")
+	var podNumberColumnWidth int = len("# of Pods")
+
+	// loop once to find out how much padding is required per column
+	for i := 0; i < len(clusterScan.Spec.Nodes); i++ {
+		nameColumnWidth = int(math.Max(float64(nameColumnWidth), float64(len(clusterScan.Spec.Nodes[i].Name))))
+		// using Itoa is definitely not the fastest way to find this but I didn't want to add a loop
+		// and a couple temp variables when I could do it with one line of code.
+		uidColumnWidth = int(math.Max(float64(uidColumnWidth), float64(len(strconv.Itoa(int(clusterScan.Spec.Nodes[i].UID))))))
+		podNumberColumnWidth = int(math.Max(float64(podNumberColumnWidth), float64(len(strconv.Itoa(int(clusterScan.Spec.Nodes[i].NumberOfPods)))+5)))
+	}
+
+	output = output + strings.Repeat(" ", 4) + "Name" + strings.Repeat(" ", nameColumnWidth-len("Name")) +
+		" | UID" + strings.Repeat(" ", uidColumnWidth-len("UID")) +
+		" | # of Pods" + strings.Repeat(" ", podNumberColumnWidth-len("# of Pods")) + " | Status\n"
+
+	// another loop to actually add the data to output string
+	for i := 0; i < len(clusterScan.Spec.Nodes); i++ {
+		var line string = ""
+		if clusterScan.Spec.Nodes[i].Master {
+			line = line + "  * "
+		} else {
+			line = line + strings.Repeat(" ", 4)
+		}
+		line = line + clusterScan.Spec.Nodes[i].Name + strings.Repeat(" ", nameColumnWidth-len(clusterScan.Spec.Nodes[i].Name)) + " | " +
+			fmt.Sprintf("%d", clusterScan.Spec.Nodes[i].UID) + strings.Repeat(" ", uidColumnWidth-len(strconv.Itoa(int(clusterScan.Spec.Nodes[i].UID)))) + " | " +
+			fmt.Sprintf("%d Pods", clusterScan.Spec.Nodes[i].NumberOfPods) + strings.Repeat(" ", podNumberColumnWidth-len(fmt.Sprintf("%d Pods", clusterScan.Spec.Nodes[i].NumberOfPods))) + " | " +
+			string(clusterScan.Spec.Nodes[i].Status)
+
+		output = output + line + "\n"
+	}
+
+	return output
 }
 
 //+kubebuilder:rbac:groups=log.my.domain,resources=clusterscans,verbs=get;list;watch;create;update;patch;delete
@@ -73,8 +121,59 @@ func (r *ClusterScanReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 	// temp log just to know that this all works
 	log.V(1).Info("loaded clusterScan object!")
 
-	// TODO: create a job that pretty-prints the data in the received ClusterScan
+	var cmdString string = prettyPrintClusterInfo(clusterScan)
+	constructLogJob := func(clusterScan *logv1.ClusterScan) (*kbatch.Job, error) {
+		name := fmt.Sprintf("clusterscanlog-%d", time.Now().Unix())
 
+		// make a string here with the pretty printed representation of our nodes
+		// and feed it into the Job's Command field and we're done! :D
+
+		job := &kbatch.Job{
+			ObjectMeta: metav1.ObjectMeta{
+				Labels:      make(map[string]string),
+				Annotations: make(map[string]string),
+				Name:        name,
+				Namespace:   clusterScan.Namespace,
+			},
+			Spec: kbatch.JobSpec{
+				Template: corev1.PodTemplateSpec{
+					Spec: corev1.PodSpec{
+						Containers: []corev1.Container{
+							{
+								Name:    name,
+								Image:   "bash",
+								Command: strings.Split(fmt.Sprintf("echo %s", cmdString), " "),
+							},
+						},
+						RestartPolicy: corev1.RestartPolicyNever,
+					},
+				},
+			},
+		}
+		if err := ctrl.SetControllerReference(clusterScan, job, r.Scheme); err != nil {
+			return nil, err
+		}
+
+		return job, nil
+	}
+
+	// construct our job
+	job, err := constructLogJob(&clusterScan)
+	if err != nil {
+		log.Error(err, "unable to construct job from template")
+		return ctrl.Result{}, nil
+	}
+
+	// create job on cluster
+	if err := r.Create(ctx, job); err != nil {
+		log.Error(err, "unable to create Job for CronJob", "job", job)
+		return ctrl.Result{}, err
+	}
+
+	// log success at highest verbosity level for readability in logs
+	log.V(1).Info("created Job for ClusterScan", "job", job)
+
+	// return nothin if the job is correctly constructed and created on cluster
 	return ctrl.Result{}, nil
 }
 
